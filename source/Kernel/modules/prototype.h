f30ceedb0041a68af4360fcdb46fb8fcca68e262
dezska <104264903+antiglory@users.noreply.github.com>
2025-11-30 18:59:39 -0300

    update prototype.h

--- source/Kernel/modules/prototype.h
+++ source/Kernel/modules/prototype.h
@@ -3,21 +3,14 @@
 
 // init.h
 TEXT  ALIGNED void init(void);
-TEXT  ALIGNED void init_map_kernel_sections(void);
 TEXT  ALIGNED void init_idt(void);
+TEXT  ALIGNED void init_pic(void);
+TEXT  ALIGNED void init_pit(void);
 
 // self
-BTEXT ALIGNED void kstart(uint64_t* pml4, uint64_t* pdpt, uint64_t* pd, uint64_t* pt)  __attribute__((naked));
+BTEXT ALIGNED void kstart() __attribute__((noreturn, naked));
 TEXT  ALIGNED void main(void);
 
 // ../../Linker/kernel.ld
-extern char __stack_start[];
-extern char __stack_end[];
-extern uintptr_t _kernel_vma;
-extern uintptr_t _kernel_lma;
-uint64_t* pt_tables;
-uint64_t* pd_table;
-uint64_t* pdpt_table;
-uint64_t* pml4_table;
 
 #endif
