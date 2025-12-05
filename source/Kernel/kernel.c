f3ec4c207be8a767a2d0745ae776af5248f5bbd5
dezska <104264903+antiglory@users.noreply.github.com>
2025-12-05 20:03:21 -0300

    update kernel.c

--- source/Kernel/kernel.c
+++ source/Kernel/kernel.c
@@ -1,4 +1,4 @@
-// 0xffffffff80000000 - 0xffffffff80200000 [pt_ffffff8000000]
+// 0xffffffff80000000 - 0xffffffff80200000
 #include <stdint.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -8,26 +8,28 @@
 #include "modules/macro.h"
 #include "modules/prototype.h"
 
-#define PAGE_SIZE 4096ULL
+#ifndef PAGE_SIZE
+    #define PAGE_SIZE 4096ULL
+#endif
 
 // self
 volatile uint64_t cpu_ticks = 0;
 
-extern char _kernel_text_start[];
-extern char _kernel_text_end[];
-extern char _kernel_rodata_start[];
-extern char _kernel_rodata_end[];
-extern char _kernel_data_start[];
-extern char _kernel_data_end[];
-extern char _kernel_bss_start[];
-extern char _kernel_bss_end[];
-extern char _kernel_stack_start[];
-extern char _kernel_stack_end[];
-extern char _kernel_heap_start[];
-extern char _kernel_heap_end[];
-extern char _kernel_vma[];
-extern char _kernel_lma[];
-extern char _kernel_vo[];
+extern uint8_t _kernel_text_start[];
+extern uint8_t _kernel_text_end[];
+extern uint8_t _kernel_rodata_start[];
+extern uint8_t _kernel_rodata_end[];
+extern uint8_t _kernel_data_start[];
+extern uint8_t _kernel_data_end[];
+extern uint8_t _kernel_bss_start[];
+extern uint8_t _kernel_bss_end[];
+extern uint8_t _kernel_stack_start[];
+extern uint8_t _kernel_stack_end[];
+extern uint8_t _kernel_heap_start[];
+extern uint8_t _kernel_heap_end[];
+extern uint8_t _kernel_vma[];
+extern uint8_t _kernel_lma[];
+extern uint8_t _kernel_vo[];
 
 // kernel entrypoint
 void kstart(void)
@@ -56,10 +58,12 @@ void kstart(void)
     __builtin_unreachable();
 }
 
+void kprintf(const unsigned char* str, ...);
+
 #include "modules/wrapper.h"
 #include "modules/io.h"
 #include "modules/string.h"
-#include "modules/tty.h"
+#include "modules/alloc.h"
 
 __attribute__((noreturn)) void panic(void)
 {
@@ -70,38 +74,32 @@ __attribute__((noreturn)) void panic(void)
     halt();
 }
 
+#include "modules/threads.h"
+#include "modules/tty.h"
 #include "modules/init.h"
-// #include "modules/alloc.h"
-// #include "modules/loader.h"
 
 void sleep(uint64_t ms)
 {
     uint64_t target = cpu_ticks + ms / 10;
-
+    
+    sti();
     while (cpu_ticks < target)
     {
         asm volatile("hlt");
     }
 }
 
-// .text -> 0xffffffff80101000
+// .text -> 0xffffffff80100100
 void main(void)
 {
     asm volatile("int $0xF0"); // beep
+    kprintf("\nv0 is alive!\n");
 
-    kprintf("hello kernel!\n");
-  
-    while (1);
+    dump_runqueue();
 
-    /*
-    char str[32];
-    while (1)
+    for (;;)
     {
-	      kprintf("> ");
-	      gets(str);
-    
-        if (strcmp(str, "ping") == 0) kprintf("pong\n");
-        else if (strcmp(str, "halt") == 0) halt();
+        // dump_runqueue();
+        kthread_yield();
     }
-    */
 }
